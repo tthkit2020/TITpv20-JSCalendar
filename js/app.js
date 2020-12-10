b378ea8c1641de7324c4a4bb1decbcacf4ef9772
Julia Aasmaa <my_e_10@hotmail.com>
2020-12-10 10:25:59 +0200

    commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const calendarButton = document.querySelector('.btn-start');
+const calendarContainer = document.querySelector('.container');
+const months = 12;
+
+calendarButton.addEventListener('click', (event) => {
+    console.log("test");
+
+    event.preventDefault();
+});
