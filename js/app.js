827fbb132f573f7bf0d4e1da113ab39fae299001
Julia Aasmaa <my_e_10@hotmail.com>
2020-12-10 11:26:45 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -5,5 +5,29 @@ const months = 12;
 calendarButton.addEventListener('click', (event) => {
     console.log("test");
 
+    for(let i = 0; i < months; i++){
+        console.log(i);
+        const calendarDoor = document.createElement('div');//генерируем новый div
+        calendarDoor.classList.add('image'); //даем div имя класса
+        calendarDoor.style.gridArea = "door"+(i+1);
+
+        const calendarDoorText = document.createElement('div');
+        calendarDoorText.classList.add('text');
+        calendarDoorText.innerHTML = i+1;
+
+        calendarDoor.appendChild(calendarDoorText);
+        calendarContainer.appendChild(calendarDoor);
+
+        let picNumber = i+1;
+        let picPath = `./img/pic${picNumber}.jpg`;
+
+        calendarDoorText.addEventListener('click', (e) => {
+            e.target.parentNode.style.backgroundImage = `url("${picPath}")`;
+            e.target.style.opacity = "0";
+            e.target.style.backgroundColor = "#333";
+            
+        });
+    }
+
     event.preventDefault();
 });
